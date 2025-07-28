cfc5c0ce68befc08a25b24b115c46866721ceaef
pdiqui-17 <quy.pham17012005@hcmut.edu.vn>
2025-07-28 16:28:23 +0700

    Update next_permua.py

--- next_permua.py
+++ next_permua.py
@@ -1,19 +1,17 @@
-def NextPermuatation(nums):
-    def single_loop(nums, index):
-        for i in range(1,len(nums)-index):
-            if nums[-i] > nums[index]:
-                nums[-i], nums[index] = nums[index], nums[-i]
-                return 0
-        return 1
-    for i in range(len(nums) - 2, -1, -1):
-      if  single_loop(nums,i) == 0: 
-          nums[i+1:] = reversed(nums[i+1:])
-          break
-a = [1,2,3]
-NextPermuatation(a)
-print(a)
-          
-
- 
-   
-    
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def single_loop(nums, index):
+            for i in range(1,len(nums)-index):
+                if nums[-i] > nums[index]:
+                    nums[-i], nums[index] = nums[index], nums[-i]
+                    return 0
+            return 1
+        for i in range(len(nums) - 2, -1, -1):
+            if  single_loop(nums,i) == 0: 
+                nums[i+1:] = reversed(nums[i+1:])
+                break
+        else:
+                  nums[:] = sorted(nums)
