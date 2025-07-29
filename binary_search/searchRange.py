dc514187d818559440c0e3c7493ca4e678027ff8
pdiqui-17 <quy.pham17012005@hcmut.edu.vn>
2025-07-29 14:11:43 +0700

    change search range

--- binary_search/searchRange.py
+++ binary_search/searchRange.py
@@ -1,7 +1,10 @@
 #This is the 34th leetcode question named : Find First and Last Position of Element in Sorted Array
 # To solve this, i try to find the first and the last occurrence of the target number in the array by the same algorithm
 # Below is how the algorithm work, i choose find function to explain, find :
-# Firstly, choose the midddle point of the array, if 
+# Firstly, choose the midddle point of the array, if the middle point is smaller than the target that means 
+# there is no target in left side of the current array, and we need to go on to the right one. 
+# On the other hand, that tells us the start of the range maybe of the left side of this point, so we search on this side
+# the function stop when the left element reach the first element of the array
 def searchRange(nums, target):
     def findFirst(nums, target):
         left, right = 0, len(nums) - 1
