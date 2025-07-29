23daf475645067195ff7975d8a5753537aed2b26
pdiqui-17 <quy.pham17012005@hcmut.edu.vn>
2025-07-29 14:07:18 +0700

    searchRange

--- binary_search/searchRange.py
+++ binary_search/searchRange.py
@@ -1,29 +1,34 @@
-def searchRange(nums,target) :
-    def findFirst(target,first,last):
-            if last - first == 1:
-                if nums[last] == target :
-                    return last
-                else : return -1
-            mid = (last + first)//2
-            if nums[mid] < target :
-                return findFirst(target,mid,last)
-            else : return findFirst(target,first,mid)
-    def findLast(target,first,last):
-        if last - first ==1:
-              if nums[first]==target:
-                   return first
-              else: return -1
-        mid = (last+first)//2
-        if nums[mid] > target:
-             return findLast(target,first,mid)
-        else : return findLast(target,mid,last)
-    if len(nums) == 0: return [-1,-1]
-    if len(nums) == 1 and nums[0] == target : return [0]
-    
-    first = findFirst(target,0,len(nums))
-    last = findLast(target,0,len(nums))
-    if first == -1 and last == -1 : return [-1,-1]
-    if first == -1 : return last
-    if last == -1 : return first
-    return [first,last]
+#This is the 34th leetcode question named : Find First and Last Position of Element in Sorted Array
+# To solve this, i try to find the first and the last occurrence of the target number in the array by the same algorithm
+# Below is how the algorithm work, i choose find function to explain, find :
+# Firstly, choose the midddle point of the array, if 
+def searchRange(nums, target):
+    def findFirst(nums, target):
+        left, right = 0, len(nums) - 1
+        res = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                if nums[mid] == target:
+                    res = mid
+                right = mid - 1
+        return res
 
+    def findLast(nums, target):
+        left, right = 0, len(nums) - 1
+        res = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                if nums[mid] == target:
+                    res = mid
+                left = mid + 1
+        return res
+
+    first = findFirst(nums, target)
+    last = findLast(nums, target)
+    return [first, last]
